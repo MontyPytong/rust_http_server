f0b25bc620cb21f732910cce6ee744c8610387c0
Cristi Ene <ene.georgec@gmail.com>
2023-02-20 19:21:29 +0200

    string demo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
-    server.run();
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];
+
+    dbg!(&string);
+    dbg!(string_slice);
+    // let server = Server::new("127.0.0.1:8080");
+    // server.run();
 }
 
 struct Server {
@@ -9,13 +14,8 @@ struct Server {
 
 impl Server {
     fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
+        Self { addr }
     }
 
-    fn run(self) {
-
-
-    }
+    fn run(self) {}
 }
