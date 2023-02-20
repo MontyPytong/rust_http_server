b0ecab511adbf00c51c5b413e6fadf7728c74448
Cristi Ene <ene.georgec@gmail.com>
2023-02-20 21:51:54 +0200

    match listener

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
+use server::Server;
+use http::Method;
+use http::Request;
+
+mod http;
+mod server;
+
 fn main() {
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];
-    let string_borrow: &str = &string;
-    let string_literal = "1234";
-
-    dbg!(&string);
-    dbg!(string_slice);
-    dbg!(string_borrow);
-    dbg!(string_literal);
-    // let server = Server::new("127.0.0.1:8080");
-    // server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {}
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
