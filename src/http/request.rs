1fb3828a08a3b7bb1502e5e97cbf72d42315024b
Cristi Ene <ene.georgec@gmail.com>
2023-03-09 22:30:02 +0200

    converted utf8 to string and added Parse Error

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,65 @@
+use std::str;
+use std::str::Utf8Error;
+use::std::convert::TryFrom;
 use super::method::Method;
+use std::error::Error;
+use std::fmt::{Display, Formatter, Debug};
+use std::fmt::Result as FmtResult;
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    //GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error>{
+
+        let request =  str::from_utf8(buf)?;
+
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Macro",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Macro",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError{
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+        
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f:&mut Formatter) -> FmtResult{
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f:&mut Formatter) -> FmtResult{
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
+
+
